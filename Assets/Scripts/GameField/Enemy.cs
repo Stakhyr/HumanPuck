8a6eff0200627b45461a52d72a952155dcc6146e
StakhurskyiGameDev <d.stakhursky@ukma.edu.ua>
2021-08-03 14:29:48 +0300

    first commit

---
+++ Assets/Scripts/GameField/Enemy.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform puck; 
+
+    [SerializeField]private float distanceToBorder = 2f;
+    public float Speed = 10f;
+    private bool movingRihgt = false;
+
+    void FixedUpdate()
+    {
+        if (TargetInRange()) 
+        {
+            MoveTotarget();
+        }
+
+        Patroll();
+        DetectSides();
+        Debug.Log(Speed);
+    }
+
+    private void Patroll()
+    {
+        if (movingRihgt == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+
+        if (movingRihgt == true)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+    }
+
+    private bool TargetInRange() 
+    {
+        if (Vector3.Distance(transform.position, puck.transform.position) < 4f)
+        {
+            return true;
+        }
+        return false;
+
+    }
+
+    void MoveTotarget() 
+    {
+        float Direction = Mathf.Sign(puck.transform.position.x - transform.position.x);
+        Vector3 MovePos = new Vector3(
+             transform.position.x + Direction * Speed * Time.deltaTime, //MoveTowards on 1 axis
+            transform.position.y,
+            transform.position.z
+        );
+        transform.position = MovePos;
+    }
+
+   //using raycasts detect distance to walls
+    void DetectSides() 
+    {
+        int layerMask = 1 << 9;
+
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, distanceToBorder, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * hit.distance, Color.red);
+            movingRihgt = true;
+        }
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, distanceToBorder, layerMask))
+        {
+            movingRihgt = false;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/UI_Parts/SceneManage.cs
@@ -0,0 +1,76 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class SceneManage : MonoBehaviour
+{
+    private int score;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    public int blocksCount;
+    [SerializeField] private Transform blockPrefab;
+    [SerializeField] private TextMeshProUGUI textMeshProUGUI;
+    private GameObject[] gos;
+    private int levelnumber = 1;
+    private Enemy enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        ShowScore();
+
+        SpawnBlock.spawnBlocks(blocksCount, blockPrefab);
+        textMeshProUGUI.SetText("Level: "+ levelnumber.ToString());
+
+        enemy = GameObject.FindWithTag("Enemy").GetComponent<Enemy>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (BlockEmount()) 
+        {
+            levelnumber += 1;
+            blocksCount += 1;
+            SpawnBlock.spawnBlocks(blocksCount, blockPrefab);
+            
+            textMeshProUGUI.SetText("Level: " + levelnumber.ToString());
+
+            enemy.Speed += 0.5f;
+
+        }
+    }
+
+    public bool BlockEmount()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Box");
+
+        if (gos.Length == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // Update is called once per frame
+    public void IncreaseScore(int points)
+    {
+        score += points;
+        ShowScore();
+    }
+
+    public void ReduceScore(int points)
+    {
+        score -= points;
+        if (score < 0) score = 0;
+        ShowScore();
+    }
+
+    private void ShowScore()
+    {
+        scoreText.SetText("Score: " + score.ToString());
+
+    }
+}
